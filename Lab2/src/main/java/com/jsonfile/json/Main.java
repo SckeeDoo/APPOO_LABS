5bc18bb0611a8b5b63b7a414af8dccc733ab6dc7
Schidu Vasile <sckeedoo@gmail.com>
2017-03-23 23:19:20 +0200

    Added dependecies to mave for parcing JSON and GSON

---
+++ Lab2/src/main/java/com/jsonfile/json/Main.java
@@ -0,0 +1,12 @@
+package com.jsonfile.json;
+
+/**
+ * Created by schiduvasile on 3/23/17.
+ */
+public class Main {
+
+    public static void main(String[] args ) {
+
+    }
+
+}
---
+++ Lab2/src/main/java/com/jsonfile/json/json/TeitchAPI.java
@@ -0,0 +1,334 @@
+package com.jsonfile.json.json;
+
+/**
+ * Created by schiduvasile on 3/23/17.
+ */
+public class TeitchAPI {
+
+    private String mc_kids;
+
+    private String bos_majority;
+
+    private String rc_total;
+
+    private String ga_adults;
+
+    private String in_total;
+
+    private String rc_kids;
+
+    private String kg_kids;
+
+    private String _2010_census_population;
+
+    private String fc_total;
+
+    private String in_kids;
+
+    private String unduplicated_persons_clients;
+
+    private String cf_total;
+
+    private String ga_kids;
+
+    private String in_adults;
+
+    private String census_tract;
+
+    private String cf_adults;
+
+    private String aa_adults;
+
+    private String aa_total;
+
+    private String ga_total;
+
+    private String place_majority;
+
+    private String cw_adults;
+
+    private String kg_total;
+
+    private String fc_kids;
+
+    private String kg_adults;
+
+    private String rc_adults;
+
+    private String cf_kids;
+
+    private String aa_kids;
+
+    private String cw_kids;
+
+    private String mc_total;
+
+    private String mc_adults;
+
+    private String cw_total;
+
+    private String fc_adults;
+
+    public String getMc_kids () {
+        return mc_kids;
+    }
+
+    public void setMc_kids (String mc_kids) {
+        this.mc_kids = mc_kids;
+    }
+
+    public String getBos_majority () {
+        return bos_majority;
+    }
+
+    public void setBos_majority (String bos_majority) {
+        this.bos_majority = bos_majority;
+    }
+
+    public String getRc_total () {
+        return rc_total;
+    }
+
+    public void setRc_total (String rc_total) {
+        this.rc_total = rc_total;
+    }
+
+    public String getGa_adults () {
+        return ga_adults;
+    }
+
+    public void setGa_adults (String ga_adults) {
+        this.ga_adults = ga_adults;
+    }
+
+    public String getIn_total () {
+        return in_total;
+    }
+
+    public void setIn_total (String in_total) {
+        this.in_total = in_total;
+    }
+
+    public String getRc_kids () {
+        return rc_kids;
+    }
+
+    public void setRc_kids (String rc_kids) {
+        this.rc_kids = rc_kids;
+    }
+
+    public String getKg_kids () {
+        return kg_kids;
+    }
+
+    public void setKg_kids (String kg_kids) {
+        this.kg_kids = kg_kids;
+    }
+
+    public String get_2010_census_population () {
+        return _2010_census_population;
+    }
+
+    public void set_2010_census_population (String _2010_census_population) {
+        this._2010_census_population = _2010_census_population;
+    }
+
+    public String getFc_total () {
+        return fc_total;
+    }
+
+    public void setFc_total (String fc_total) {
+        this.fc_total = fc_total;
+    }
+
+    public String getIn_kids () {
+        return in_kids;
+    }
+
+    public void setIn_kids (String in_kids) {
+        this.in_kids = in_kids;
+    }
+
+    public String getUnduplicated_persons_clients () {
+        return unduplicated_persons_clients;
+    }
+
+    public void setUnduplicated_persons_clients (String unduplicated_persons_clients) {
+        this.unduplicated_persons_clients = unduplicated_persons_clients;
+    }
+
+    public String getCf_total () {
+        return cf_total;
+    }
+
+    public void setCf_total (String cf_total) {
+        this.cf_total = cf_total;
+    }
+
+    public String getGa_kids () {
+        return ga_kids;
+    }
+
+    public void setGa_kids (String ga_kids) {
+        this.ga_kids = ga_kids;
+    }
+
+    public String getIn_adults () {
+        return in_adults;
+    }
+
+    public void setIn_adults (String in_adults) {
+        this.in_adults = in_adults;
+    }
+
+    public String getCensus_tract () {
+        return census_tract;
+    }
+
+    public void setCensus_tract (String census_tract) {
+        this.census_tract = census_tract;
+    }
+
+    public String getCf_adults () {
+        return cf_adults;
+    }
+
+    public void setCf_adults (String cf_adults) {
+        this.cf_adults = cf_adults;
+    }
+
+    public String getAa_adults () {
+        return aa_adults;
+    }
+
+    public void setAa_adults (String aa_adults) {
+        this.aa_adults = aa_adults;
+    }
+
+    public String getAa_total () {
+        return aa_total;
+    }
+
+    public void setAa_total (String aa_total) {
+        this.aa_total = aa_total;
+    }
+
+    public String getGa_total () {
+        return ga_total;
+    }
+
+    public void setGa_total (String ga_total) {
+        this.ga_total = ga_total;
+    }
+
+    public String getPlace_majority () {
+        return place_majority;
+    }
+
+    public void setPlace_majority (String place_majority) {
+        this.place_majority = place_majority;
+    }
+
+    public String getCw_adults () {
+        return cw_adults;
+    }
+
+    public void setCw_adults (String cw_adults) {
+        this.cw_adults = cw_adults;
+    }
+
+    public String getKg_total () {
+        return kg_total;
+    }
+
+    public void setKg_total (String kg_total) {
+        this.kg_total = kg_total;
+    }
+
+    public String getFc_kids () {
+        return fc_kids;
+    }
+
+    public void setFc_kids (String fc_kids) {
+        this.fc_kids = fc_kids;
+    }
+
+    public String getKg_adults () {
+        return kg_adults;
+    }
+
+    public void setKg_adults (String kg_adults) {
+        this.kg_adults = kg_adults;
+    }
+
+    public String getRc_adults () {
+        return rc_adults;
+    }
+
+    public void setRc_adults (String rc_adults) {
+        this.rc_adults = rc_adults;
+    }
+
+    public String getCf_kids () {
+        return cf_kids;
+    }
+
+    public void setCf_kids (String cf_kids) {
+        this.cf_kids = cf_kids;
+    }
+
+    public String getAa_kids () {
+        return aa_kids;
+    }
+
+    public void setAa_kids (String aa_kids) {
+        this.aa_kids = aa_kids;
+    }
+
+    public String getCw_kids () {
+        return cw_kids;
+    }
+
+    public void setCw_kids (String cw_kids) {
+        this.cw_kids = cw_kids;
+    }
+
+    public String getMc_total () {
+        return mc_total;
+    }
+
+    public void setMc_total (String mc_total) {
+        this.mc_total = mc_total;
+    }
+
+    public String getMc_adults () {
+        return mc_adults;
+    }
+
+    public void setMc_adults (String mc_adults) {
+        this.mc_adults = mc_adults;
+    }
+
+    public String getCw_total () {
+        return cw_total;
+    }
+
+    public void setCw_total (String cw_total) {
+        this.cw_total = cw_total;
+    }
+
+    public String getFc_adults () {
+        return fc_adults;
+    }
+
+    public void setFc_adults (String fc_adults) {
+        this.fc_adults = fc_adults;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [mc_kids = "+mc_kids+", bos_majority = "+bos_majority+", rc_total = "+rc_total+", ga_adults = "+ga_adults+", in_total = "+in_total+", rc_kids = "+rc_kids+", kg_kids = "+kg_kids+", _2010_census_population = "+_2010_census_population+", fc_total = "+fc_total+", in_kids = "+in_kids+", unduplicated_persons_clients = "+unduplicated_persons_clients+", cf_total = "+cf_total+", ga_kids = "+ga_kids+", in_adults = "+in_adults+", census_tract = "+census_tract+", cf_adults = "+cf_adults+", aa_adults = "+aa_adults+", aa_total = "+aa_total+", ga_total = "+ga_total+", place_majority = "+place_majority+", cw_adults = "+cw_adults+", kg_total = "+kg_total+", fc_kids = "+fc_kids+", kg_adults = "+kg_adults+", rc_adults = "+rc_adults+", cf_kids = "+cf_kids+", aa_kids = "+aa_kids+", cw_kids = "+cw_kids+", mc_total = "+mc_total+", mc_adults = "+mc_adults+", cw_total = "+cw_total+", fc_adults = "+fc_adults+"]";
+    }
+
+}
